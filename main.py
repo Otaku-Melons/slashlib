48f1126140334a664d213d6303648ef60ac824c4
DUB1401 <vlad.milosta@outlook.com>
2024-06-06 11:14:46 +0300

    Project upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from Parsers.mangalib.main import VERSION, STRUCT
+from Parsers.mangalib.main import Parser
+
+import Parsers.mangalib.main as main 
+
+#==========================================================================================#
+# >>>>> ОПРЕДЕЛЕНИЯ <<<<< #
+#==========================================================================================#
+
+NAME = "slashlib"
+REPOS = "https://github.com/DUB1401/slashlib"
+SITE = "test-front.slashlib.me"
+main.NAME = NAME
+main.SITE = SITE
