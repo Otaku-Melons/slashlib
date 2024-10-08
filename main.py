aa657cecb7e2466bd2d17b2ab225e3753c109607
DUB1401 <vlad.milosta@outlook.com>
2024-10-08 21:45:18 +0300

    Fix: struct

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Parsers.mangalib.main import VERSION, STRUCT
+from Parsers.mangalib.main import VERSION, TYPE
 from Parsers.mangalib.main import Parser
 
 import Parsers.mangalib.main as main 
@@ -8,7 +8,6 @@ import Parsers.mangalib.main as main
 #==========================================================================================#
 
 NAME = "slashlib"
-REPOS = "https://github.com/DUB1401/slashlib"
 SITE = "slashlib.me"
 main.NAME = NAME
 main.SITE = SITE
